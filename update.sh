cc91bea6bba5a520be1621fb94df0a9a1a4b85b5
c-holtermann <mail@c-holtermann.net>
2020-04-10 12:26:12 +0200

    email figures

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ python3 plot_cases.py
 python3 plot_deaths.py
 printf -v date '%(%Y%m%d)T' -1
 sed -i 's/-[0-9]\{8\}/-'$date'/g' README.md
+sed -i 's/-[0-9]\{8\}/-'$date'/g' email_template.html
 git add -A
 git commit -m "update $date"
 git push
+mailx -a 'Content-Type: text/html' -s "Covid 19 figures $date" mail@c-holtermann.net < email_template.html
